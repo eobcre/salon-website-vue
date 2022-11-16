f118afac6aed759ad2009a28b02c3ff1f31c6336
eobcre <eobcre@gmail.com>
2022-11-16 18:33:17 -0500

    First commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <small>&copy; 2023 Emmy</small>
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div>
+      <!-- <img src="" alt="Logo" /> -->
+      <p>Logo</p>
+    </div>
+    <nav>
+      <li>Salon</li>
+      <li>Gallery</li>
+      <li>Book</li>
+    </nav>
+  </div>
+</template>
+
+<style scoped></style>

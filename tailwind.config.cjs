0d52b7a9c51487a9ee8fcfe60c21e2af89077fce
eobcre <eobcre@gmail.com>
2022-11-24 14:38:59 -0500

    Styles added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-gray': '#303030',
+      },
+    },
   },
   plugins: [],
 };

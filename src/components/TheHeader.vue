b0a100bc3317cff9589757a04108fa9cfd16b275
eobcre <eobcre@gmail.com>
2022-11-24 14:40:10 -0500

    Styles

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,5 +3,24 @@ export default {};
 </script>
 
 <template>
-  <div>Logo</div>
+  <div
+    class="flex justify-between items-center text-dark-gray w-3/4 mx-auto border-b-2 border-dark-gray py-6"
+  >
+    <router-link :to="{ path: '/' }">Logo</router-link>
+    <nav>
+      <ul class="flex space-x-4">
+        <li>
+          <router-link :to="{ name: 'TheSalon' }">Salon</router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'TheGallery' }">Gallery</router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'TheBook' }">Book Online</router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
+
+<style scoped></style>
